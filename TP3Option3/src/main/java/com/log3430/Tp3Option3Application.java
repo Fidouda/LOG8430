4258df9bde56d7445680154fa56e45bec8563e6a
Julien Aymong <julien.aymong@gmail.com>
2016-03-30 13:30:36 -0400

    Add basic Springboot server

---
+++ TP3Option3/src/main/java/com/log3430/Tp3Option3Application.java
@@ -0,0 +1,12 @@
+package com.log3430;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3Option3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3Option3Application.class, args);
+	}
+}
---
+++ TP3Option3/src/test/java/com/log3430/Tp3Option3ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.log3430;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Tp3Option3Application.class)
+public class Tp3Option3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
